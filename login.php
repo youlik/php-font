5a4e631fe804ad5592959a2ffda470de166a149b
Administrator <1454159@qq.com>
2019-01-07 14:20:52 +0800

    1.7session存储

--- login.php
+++ login.php
@@ -5,10 +5,14 @@ header('Access-Control-Allow-Headers:Content-Type');
 header('Access-Control-Allow-Origin:http://10.30.29.65:63341');
 $data=file_get_contents("php://input");
 $data=json_decode($data);
+session_start();
 $message=array('message'=>'success');
+
 if(($data->userName==123)&&($data->passWord==123))
 {
     echo json_encode($message);
+    $_SESSION['name']=$data->userName;
+    $_SESSION['password']=$data->passWord;
 }
 else
 {
--- logout.php
+++ logout.php
@@ -1,13 +1,8 @@
 <?php
 header("Access-Control-Allow-Credentials:true");
 header('Content-type:text/json');
+header('Access-Control-Allow-Headers:Content-Type');
 header('Access-Control-Allow-Origin:http://10.30.29.65:63341');
-
-//$data_json= ;
-//echo $data_json;
-session_start();
-$username=$_SESSION['name'];
-$data=array('userName'=>$username);
-echo json_encode($data);
-
+$message=array('message'=>'success');
+echo json_encode($message);
 ?>
