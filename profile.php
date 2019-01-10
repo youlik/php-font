7ed7d633189494fbe8c0f70790c2a6e23aecebfa
Administrator <17893455@qq.com>
2019-01-10 08:21:38 +0800

    php多跨域

--- profile.php
+++ profile.php
@@ -1,8 +1,16 @@
 <?php
 header("Access-Control-Allow-Credentials:true");
 header('Content-type:text/json');
-header('Access-Control-Allow-Origin:http://10.30.29.65:63341');
-
+header('Access-Control-Allow-Headers:Content-Type');
+//header('Access-Control-Allow-Origin:http://10.30.29.65:63341');
+$allow_origin=array(
+    'http://10.30.29.65:63341',
+    'http://10.30.29.66:63341'
+);
+$origin=isset($_SERVER['HTTP_ORIGIN'])?$_SERVER['HTTP_ORIGIN']:'';
+if(in_array($origin, $allow_origin)){
+    header('Access-Control-Allow-Origin:'.$origin);
+}
 //$data_json= ;
 //echo $data_json;
 session_start();
