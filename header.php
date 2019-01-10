44cf7bee0639461a8e5c3e6178f6528cdcfe5133
Administrator <17893455@qq.com>
2019-01-10 08:48:03 +0800

    php添加 header require 直接调用 节省代码空间

---
+++ header.php
@@ -0,0 +1,14 @@
+<?php
+header("Access-Control-Allow-Credentials:true");
+header('Content-type:text/json');
+header('Access-Control-Allow-Headers:Content-Type');
+//header('Access-Control-Allow-Origin:http://10.30.29.65:63341');
+$allow_origin=array(
+    'http://10.30.29.65:63341',
+    'http://10.30.29.66:63341'
+);
+$origin=isset($_SERVER['HTTP_ORIGIN'])?$_SERVER['HTTP_ORIGIN']:'';
+if(in_array($origin, $allow_origin)) {
+    header('Access-Control-Allow-Origin:' . $origin);
+};
+?>
